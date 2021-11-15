b9df80f9da43413e32e448e0efbd0ca7b840ca69
Daniel <wuyh512@foxmail.com>
2021-11-15 14:43:21 +0800

    嵌套路由

--- vue-cli-train/hello-vue/src/router/index.js
+++ vue-cli-train/hello-vue/src/router/index.js
@@ -4,16 +4,23 @@ import Router from 'vue-router'
 import Main from "../views/Main";
 import Login from "../views/Login";
 
+import UserList from "../views/user/List";
+import UserProfile from "../views/user/Profile";
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-  {
-    path: '/main',
-    component: Main,
-  },{
-    path: '/login',
-    component: Login,
-  }
+    {
+      path: '/main',
+      component: Main,
+      children: [
+        {path: '/user/profile', component: UserProfile},
+        {path: '/user/list', component: UserList},
+      ]
+    }, {
+      path: '/login',
+      component: Login, // 嵌套路由
+    }
   ]
 })
